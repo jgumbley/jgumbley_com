2d03f00b569ba2067b3ac54088a60211b4bbc4c4
Jim Gumbley <jgumbley@thoughtworks.com>
2014-11-21 13:00:18 +0000

    need to upload txt as well

--- upload.py
+++ upload.py
@@ -10,7 +10,7 @@ config['S3_BUCKET'] = 'www.jgumbley.com'
 def config_value(key):
     return config[key]
 
-ext_allowed = tuple('jpg jpeg png html js css pdf gif'.split())
+ext_allowed = tuple('txt jpg jpeg png html js css pdf gif'.split())
 
 def allowed(filename):
     return (extension(filename) in ext_allowed)
